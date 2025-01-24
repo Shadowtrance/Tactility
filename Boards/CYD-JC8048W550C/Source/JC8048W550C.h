95843c66d197eb9a925487d538901c66eaee1bf4
Shadowtrance <teronskyn@gmail.com>
2025-01-25 07:25:40 +1000

    Update JC8048W550C.h

--- Boards/CYD-JC8048W550C/Source/JC8048W550C.h
+++ Boards/CYD-JC8048W550C/Source/JC8048W550C.h
@@ -2,5 +2,5 @@
 
 #include "hal/Configuration.h"
 
-// Capacitive touch version of the 5" yellow board
+// Capacitive touch version of the 5" black board
 extern const tt::hal::Configuration cyd_jc8048w550c_config;
