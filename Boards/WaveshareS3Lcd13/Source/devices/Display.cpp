32334d267f8a4c79c6a2e836cd39cde13d805f17
Shadowtrance <teronskyn@gmail.com>
2025-09-13 17:19:08 +1000

    test build
    new boards

---
+++ Boards/WaveshareS3Lcd13/Source/devices/Display.cpp
@@ -0,0 +1,25 @@
+#include "Display.h"
+
+#include <PwmBacklight.h>
+#include <St7789Display.h>
+
+std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay() {
+
+    auto configuration = std::make_unique<St7789Display::Configuration>(
+        SPI2_HOST,
+        GPIO_NUM_39,
+        GPIO_NUM_38,
+        240,
+        240,
+        nullptr,
+        false,
+        false,
+        false,
+        true
+    );
+
+    configuration->backlightDutyFunction = driver::pwmbacklight::setBacklightDuty;
+
+    auto display = std::make_shared<St7789Display>(std::move(configuration));
+    return std::reinterpret_pointer_cast<tt::hal::display::DisplayDevice>(display);
+}
