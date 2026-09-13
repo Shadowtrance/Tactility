65ee2238764d4e860fc8a3484c26b533b3975c90
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-09-14 00:06:55 +0200

    CL-32 drivers & more (#652)
    - CL-32 features added:
      - Hardware revision detection
      - v0.3 and v0.4 keyboard 
      - v0.4 power supply
      - Inlined epd library (was a module) and made changes to the device definition (don't do full update after 5 small updates)
    - Statusbar: add support for inverted colors
    - e-paper driver improvements and warning fixes
    - Implemented gpio_controller_get_level()
    - Keyboard support improvements (unicode for tca drivers)

--- Platforms/platform-esp32/source/module.cpp
+++ Platforms/platform-esp32/source/module.cpp
@@ -84,8 +84,6 @@ extern "C" {
     // GCC integer arithmetic helpers (needed on 32-bit targets for 64-bit ops)
     long long __divdi3(long long a, long long b);
     long long __moddi3(long long a, long long b);
-    unsigned long long __udivdi3(unsigned long long a, unsigned long long b);
-    unsigned long long __umoddi3(unsigned long long a, unsigned long long b);
 #else
     extern double __adddf3(double a, double b);
     extern double __subdf3(double a, double b);
@@ -120,6 +118,9 @@ extern "C" {
     // GCC 64-bit integer arithmetic helpers (needed for 64-bit div on 32-bit RISC-V)
     long long __divdi3(long long a, long long b);
     unsigned long long __udivdi3(unsigned long long a, unsigned long long b);
+    unsigned long long (__atomic_load_8)(const volatile void*, int);
+    void (__atomic_store_8)(volatile void*, unsigned long long, int);
+    unsigned long long (__atomic_exchange_8)(volatile void*, unsigned long long, int);
 #endif
 }
 
@@ -284,8 +285,6 @@ static const ModuleSymbol platform_esp32_symbols[] = {
     DEFINE_MODULE_SYMBOL(__gtdf2),
     DEFINE_MODULE_SYMBOL(__divdi3),
     DEFINE_MODULE_SYMBOL(__moddi3),
-    DEFINE_MODULE_SYMBOL(__udivdi3),
-    DEFINE_MODULE_SYMBOL(__umoddi3),
 #else
     DEFINE_MODULE_SYMBOL(__adddf3),
     DEFINE_MODULE_SYMBOL(__subdf3),
@@ -317,6 +316,9 @@ static const ModuleSymbol platform_esp32_symbols[] = {
     DEFINE_MODULE_SYMBOL(__clzsi2),
     DEFINE_MODULE_SYMBOL(__divdi3),
     DEFINE_MODULE_SYMBOL(__udivdi3),
+    DEFINE_MODULE_SYMBOL(__atomic_load_8),
+    DEFINE_MODULE_SYMBOL(__atomic_store_8),
+    DEFINE_MODULE_SYMBOL(__atomic_exchange_8),
 #endif
     MODULE_SYMBOL_TERMINATOR,
 };
